c92cf063dd1b634584777bd4eeb3f98d1c26c364
ds17 <ingsed@outlook.com>
2021-04-12 19:46:28 +0800

    upload saec

--- 01_Books/06_CPP_Primer/Chapter03/3.3.3.cpp
+++ 01_Books/06_CPP_Primer/Chapter03/3.3.3.cpp
@@ -6,16 +6,30 @@
 using namespace std;
 
 int main(){
-
+    //下标方法实现
     vector<unsigned> vec(11, 0);
     unsigned grade;
-    while (cin >> grade){
-        ++vec[grade/10];
-    }
 
+    // while (cin >> grade){
+    //     ++vec[grade/10];
+    // }
+
+    // for (auto item : vec){
+    //     cout << item << endl;
+    // }
+
+    //迭代器运算方法实现
+    auto it = vec.begin();
+    auto temp =it;
+    while (cin >> grade)
+    {
+        temp = it + grade/10;
+        ++(*temp);
+    }
     for (auto item : vec){
         cout << item << endl;
     }
+    
 
     cout << endl << endl;
     system("pause");
