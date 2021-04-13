605b84cdc60d42763834b9891bdcdacc032ba1f3
ds17 <ingsed@outlook.com>
2021-04-13 22:06:18 +0800

    update

---
+++ 04_Online_Courses/01_xuetang_tsinghua_C++/eg_2.6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+//把一个数顺序颠倒，345 → 543
+    int n, right_digits;
+    cout << "enter the number: ";
+    cin >> n;
+    cout << "the number of reverse order is: ";
+    do
+    {
+        right_digits = n % 10;
+        cout << right_digits;
+        n /=10;
+    } while (n!=0);
+     
+    
+    return 0;
+}
