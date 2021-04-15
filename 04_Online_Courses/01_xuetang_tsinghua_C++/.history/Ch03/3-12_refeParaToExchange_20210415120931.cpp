97402a728670925e4e8cd96d567a5d03c3430f46
ds17 <ingsed@outlook.com>
2021-04-15 16:53:33 +0800

    update

---
+++ 04_Online_Courses/01_xuetang_tsinghua_C++/.history/Ch03/3-12_refeParaToExchange_20210415120931.cpp
@@ -0,0 +1,22 @@
+//形参用引用，进而实现参数的双向传递
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b){
+    int t = a;
+    a= b;
+    b = t;
+}
+
+int main(){
+    cout << "input two integer: ";
+    int x,y;
+    cin >> x >> y;
+    cout << "x=" << x  << "y= " << y << endl;
+    swap(x, y);
+    cout << "x=" << x  << "y= " << y << endl;
+
+    cout << endl << endl;
+    system("pause");
+    return 0;
+}
