03e91df08bb662a698541ff27f1fbceee11c4457
ds17 <ingsed@outlook.com>
2021-04-21 21:41:48 +0800

    update

--- 01_Books/06_CPP_Primer/test.cpp
+++ 01_Books/06_CPP_Primer/test.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 int main(){
 
-    int a[] = {1,2,3,4,5,6,7,8,9,10};
-    int (&arrRef)[10] =a;
-    for (auto item : arrRef){
-        cout << item << "\n" ;
-    }
+    int i = 1;
+    int *p = &i;
+    int **pp = &p;
+
+    cout << p <<"\n";
+    cout << pp << "\n";
     cout << "\n\n";
     system("pause");
     return 0;
